38923b95db36ae455a8c0d19b5238ec0a53a579d
Kirill-Korzunov <kirillkorzunov@gmail.com>
2023-04-26 12:15:26 +0300

    mod new relationship

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 import prompt
 
+
 def welcome_user():
     name = prompt.string('Могу я узнать твое имя? ')
     print(f'Привет, {name}!')
+    return name
+
 
 def main():
     print('Добро пожаловать в игры разума!')
-    welcome_user()
+
 
 if __name__ == '__main__':
     main()
-    
-    
